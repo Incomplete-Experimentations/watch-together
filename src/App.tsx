919219a3b5efd891c2e963b595376a195ce84648
Joseph Arakelian <jarakelian1@chainbridgetech.com>
2022-06-07 15:21:54 -0400

    Setting up project and getting an initial frame set up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Divider, Grid, GridColumn, Input, Menu, MenuItem, MenuMenu, Segment} from "semantic-ui-react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Segment className={"App"} color={"black"} inverted={true}>
+            <Menu pointing={true} borderless={true} inverted={true} color={"grey"} fixed={"top"}>
+                <MenuMenu position={"right"}>
+                    <MenuItem>
+                        <Input icon={"search"} placeholder={"Twitch Streamer..."} />
+                    </MenuItem>
+                    <MenuItem>
+                        <Input icon={"search"} placeholder={"Embed Code..."} />
+                    </MenuItem>
+                    <MenuItem>
+                        <Button color={"blue"}>Watch</Button>
+                    </MenuItem>
+                </MenuMenu>
+            </Menu>
+            <Segment className={"content"} color={"grey"} inverted={true}>
+                <Grid columns={2} relaxed={'very'}>
+                    <GridColumn className={"youtube-wrapper"}>
+youtube
+                    </GridColumn>
+                    <GridColumn className={"twitch-wrapper"}>
+twitch
+                    </GridColumn>
+                </Grid>
+
+                <Divider vertical={true} className={"divider-position"} inverted={true}/>
+            </Segment>
+        </Segment>
+    );
 }
 
 export default App;
